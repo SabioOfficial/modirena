7cbaad5e5df93183bfdfe4bac8718c7402cf9b05
Sabio <gamester.sad@gmail.com>
2026-03-15 07:58:43 +0000

    Initial Commit

---
+++ src/main/java/net/sabio/modirena/Modirena.java
@@ -0,0 +1,10 @@
+package net.sabio.modirena;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Modirena implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
