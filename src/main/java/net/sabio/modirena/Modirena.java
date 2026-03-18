19f564a7a3a32e967da70ffc581a84bf03d63937
Sabio <gamester.sad@gmail.com>
2026-03-18 18:08:54 +0000

    Initial commits

--- src/main/java/net/sabio/modirena/Modirena.java
+++ src/main/java/net/sabio/modirena/Modirena.java
@@ -2,9 +2,12 @@ package net.sabio.modirena;
 
 import net.fabricmc.api.ModInitializer;
 
-public class Modirena implements ModInitializer {
+import java.util.logging.Logger;
 
+public class Modirena implements ModInitializer {
+    public static final Logger LOGGER = Logger.getLogger(ModirenaConstants.MOD_ID);
     @Override
     public void onInitialize() {
+        LOGGER.info("modirena is running successfully");
     }
 }
---
+++ src/main/java/net/sabio/modirena/ModirenaConstants.java
@@ -0,0 +1,6 @@
+package net.sabio.modirena;
+
+public final class ModirenaConstants {
+    public static final String MOD_ID = "modirena";
+    private ModirenaConstants() {}
+}
